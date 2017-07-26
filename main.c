512080df159b20e2fe512acede3133a6c8e45624
hiforujitaaki <hirofujitaaki.gmail.com>
2017-07-26 12:48:18 +0900

    add: hiding the cursor

--- main.c
+++ main.c
@@ -2,18 +2,24 @@
 #include <time.h>
 
 #define clearScreen() printf("\033[2J")
-#define setPosition(x,y) printf("\033[%d;%dH",(y),(x))
+#define setPosition(x,y) printf("\033[%d;%dH",(y)+1, (x)*2+1)  //adjustment for indexing
 #define setCharColor(n) printf("\033[3%dm",(n))
 #define setBackColor(n) printf("\033[4%dm",(n))
 #define setAttribute(n) printf("\033[%dm",(n))
+#define cursolOn() printf("\033[?25h")  // displays the cursor
+#define cursolOff() printf("\033[?25l")  //hides the cursor
 
 #define BLACK 0
 #define RED 1
 #define WHITE 7
 #define DEFAULT 9
 
+/* for setAttribute() */
 #define NORMAL 0
 #define REVERSE 7
+
+#define WIDTH 10
+#define HEIGHT 20
 
 
 
@@ -23,6 +29,7 @@ int main(int argc, char *argv[])
 {
     int y;
     clearScreen();
+    cursolOff();
     for (y=1; y<20; y++)
     {
         setPosition(5, y);
@@ -32,7 +39,7 @@ int main(int argc, char *argv[])
         printf("  ");
         fflush(stdout);
 
-        wait(500);
+        wait(250);
 
         setPosition(5, y);
         setCharColor(WHITE);
@@ -44,6 +51,7 @@ int main(int argc, char *argv[])
 
     setBackColor(DEFAULT);
     setCharColor(DEFAULT);
+    cursolOn();
 }
 
 int wait(int msec)
